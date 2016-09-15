a7e2525513a96ab32c21d07109beeee417b5479f
7kia <il7kia@yandex.ru>
2016-09-15 14:47:16 +0300

    Update tests

--- Triangle/x64/Debug/BATTests.bat
+++ Triangle/x64/Debug/BATTests.bat
@@ -1,11 +1,99 @@
-Triangle.exe
+echo Не правильное количество аргументов
+Triangle.exe > incorrectAmountArgument1.txt
+
 IF NOT ERRORLEVEL 1 GOTO err
 
-Triangle.exe 1
+FC /b incorrectAmountArgument1.txt CheckData/invalidArguments.txt
+
+
+
+Triangle.exe 1 > incorrectAmountArgument2.txt
+
 IF NOT ERRORLEVEL 1 GOTO err
 
-Triangle.exe 1 2
+FC /b incorrectAmountArgument2.txt CheckData/invalidArguments.txt
+
+
+
+Triangle.exe 1 2 > incorrectAmountArgument3.txt
+
 IF NOT ERRORLEVEL 1 GOTO err
+
+FC /b incorrectAmountArgument3.txt CheckData/invalidArguments.txt
+
+
+
+Triangle.exe 1 2 3 4 > incorrectAmountArgument4.txt
+
+IF NOT ERRORLEVEL 1 GOTO err
+
+FC /b incorrectAmountArgument4s.txt CheckData/invalidArguments.txt
+
+
+
+echo Точка допустимый , символ но одна точка не число
+Triangle.exe . 2 1  > notNumeric1.txt
+
+IF NOT ERRORLEVEL 1 GOTO err
+
+FC /b notNumeric1.txt CheckData/invalidArguments.txt
+
+Triangle.exe 2 . 1  > notNumeric2.txt
+
+IF NOT ERRORLEVEL 1 GOTO err
+
+FC /b notNumeric2.txt CheckData/invalidArguments.txt
+
+Triangle.exe 3 2 .  > notNumeric3.txt
+
+IF NOT ERRORLEVEL 1 GOTO err
+
+FC /b notNumeric3.txt CheckData/invalidArguments.txt
+
+
+echo В числе не может быть несколько точек
+Triangle.exe 0..2 2 1  > notNumeric4.txt
+
+IF NOT ERRORLEVEL 1 GOTO err
+
+FC /b notNumeric4.txt CheckData/invalidArguments.txt
+
+Triangle.exe 2 9.4.2 1  > notNumeric5.txt
+
+IF NOT ERRORLEVEL 1 GOTO err
+
+FC /b notNumeric5.txt CheckData/invalidArguments.txt
+
+Triangle.exe 3 2 1.2..2  > notNumeric6.txt
+
+IF NOT ERRORLEVEL 1 GOTO err
+
+FC /b notNumeric6.txt CheckData/invalidArguments.txt
+
+
+
+echo Аргументом могут быть только числа
+Triangle.exe a 2 1  > notNumeric7.txt
+
+IF NOT ERRORLEVEL 1 GOTO err
+
+FC /b notNumeric7.txt CheckData/invalidArguments.txt
+
+Triangle.exe 2 + 1  > notNumeric8.txt
+
+IF NOT ERRORLEVEL 1 GOTO err
+
+FC /b notNumeric8.txt CheckData/invalidArguments.txt
+
+Triangle.exe 3 2 _  > notNumeric9.txt
+
+IF NOT ERRORLEVEL 1 GOTO err
+
+FC /b notNumeric9.txt CheckData/invalidArguments.txt
+
+
+
+
 
 # >> Degenerate1.txt
 Triangle.exe -1 2 3 >> Degenerate1.txt
